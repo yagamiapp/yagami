6311048d374eb163059849cfc936f654e57de727
clxxiii <earthnoartisjusteh@gmail.com>
2022-03-23 20:18:56 -0400

    this works now

--- discord/commands/mappool.js
+++ discord/commands/mappool.js
@@ -1,22 +1,36 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { CommandInteraction } = require("discord.js");
-const options = {};
+const { CommandInteraction, MessageEmbed } = require("discord.js");
+const options = require("../../settings.json");
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("mappool")
 		.setDescription("Lists maps in a given mappool")
-		.addStringOption((option) => {
+		.addStringOption((option) =>
 			option
-				.setName("Mappool ID")
+				.setName("id")
 				.setDescription("Required to identify the pool")
-				.setRequired(true);
-		}),
+				.setRequired(true)
+		),
 	/**
 	 *
 	 * @param {CommandInteraction} interaction
 	 */
 	async execute(interaction) {
-		await interaction.reply();
+		let id = interaction.options.getString("id");
+
+		let mappool = options.rounds.filter((obj) => obj.acronym == id);
+
+		if (mappool.length == 0) {
+			await interaction.editReply({
+				content: `No Mappool with name that found.`,
+			});
+		} else {
+			let embed = new MessageEmbed()
+				.setColor("#0EB8B9")
+				.setTitle(mappool[0].name)
+				.setDescription("This is a pool");
+			await interaction.editReply({ embeds: [embed] });
+		}
 	},
 };
