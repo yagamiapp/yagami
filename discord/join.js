c86069d85a0ac6c27141b445947aa15733255653
clxxiii <earthnoartisjusteh@gmail.com>
2022-03-29 10:03:58 -0400

    no need to add template on join

--- discord/join.js
+++ discord/join.js
@@ -1,15 +1,11 @@
 let firebase = require("../firebase");
 let { Guild } = require("discord.js");
-let template = require("../templates/guild.json");
 let deploy = require("./deploy-commands");
 
 /**
- * @desc Deploys commands to guild on 
+ * @desc Deploys commands to guild on join
  * @param { Guild } ev
  */
 module.exports.onJoin = async (ev) => {
-	if ((await firebase.getData("guilds", ev.id)) == null) {
-		await firebase.setData(template, "guilds", ev.id);
-	}
 	deploy.deployCommands(ev.id);
 };
