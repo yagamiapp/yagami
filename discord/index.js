24ebed8b621b99cd481712ad7006dc6c081ae051
clxxiii <earthnoartisjusteh@gmail.com>
2022-03-30 14:34:52 -0400

    add getBot function for other module use

--- discord/index.js
+++ discord/index.js
@@ -4,55 +4,62 @@ const fs = require("fs");
 const join = require("./join");
 require("dotenv").config();
 
-module.exports.init = () => {
-	const bot = new Client({
-		intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES],
-	});
+const bot = new Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES],
+});
 
-	bot.commands = new Collection();
-	const commandFiles = fs
-		.readdirSync("./discord/commands")
-		.filter((file) => file.endsWith(".js"));
+module.exports = {
+	init() {
+		bot.commands = new Collection();
+		const commandFiles = fs
+			.readdirSync("./discord/commands")
+			.filter((file) => file.endsWith(".js"));
 
-	for (const file of commandFiles) {
-		const command = require(`./commands/${file}`);
-		// Set a new item in the Collection
-		// With the key as the command name and the value as the exported module
-		bot.commands.set(command.data.name, command);
-	}
-
-	// Command updating for testing purposes
-	commandUpdate.deployCommands(process.env.testGuildId);
-
-	bot.on("interactionCreate", async (interaction) => {
-		if (!interaction.isCommand()) return;
-
-		const command = bot.commands.get(interaction.commandName);
-
-		if (!command) return;
-
-		try {
-			if (command.defer) {
-				await interaction.deferReply({ ephemeral: command.ephemeral });
-			}
-			await command.execute(interaction);
-		} catch (error) {
-			console.error(error);
-			await interaction.editReply({
-				content: "There was an error while executing this command!",
-				ephemeral: true,
-			});
+		for (const file of commandFiles) {
+			const command = require(`./commands/${file}`);
+			// Set a new item in the Collection
+			// With the key as the command name and the value as the exported module
+			bot.commands.set(command.data.name, command);
 		}
-	});
 
-	// Setup server on join
-	bot.on("guildCreate", (ev) => {
-		join.onJoin(ev);
-	});
+		// Command updating for testing purposes
+		commandUpdate.deployCommands(process.env.testGuildId);
 
-	bot.once("ready", () => {
-		console.log("Connected to Discord!");
-	});
+		bot.on("interactionCreate", async (interaction) => {
+			if (!interaction.isCommand()) return;
 
-	bot.login(process.env.discordToken);
+			const command = bot.commands.get(interaction.commandName);
+
+			if (!command) return;
+
+			try {
+				if (command.defer) {
+					await interaction.deferReply({
+						ephemeral: command.ephemeral,
+					});
+				}
+				await command.execute(interaction);
+			} catch (error) {
+				console.error(error);
+				await interaction.editReply({
+					content: "There was an error while executing this command!",
+					ephemeral: true,
+				});
+			}
+		});
+
+		// Setup server on join
+		bot.on("guildCreate", (ev) => {
+			join.onJoin(ev);
+		});
+
+		bot.once("ready", () => {
+			console.log("Connected to Discord!");
+		});
+
+		bot.login(process.env.discordToken);
+	},
+	getBot() {
+		return bot;
+	},
 };
