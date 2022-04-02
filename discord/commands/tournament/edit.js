03c9a418c66ea5f862f56f5d72dd40b2a44e8194
clxxiii <earthnoartisjusteh@gmail.com>
2022-04-01 22:59:32 -0400

    edit command done

--- discord/commands/tournament/edit.js
+++ discord/commands/tournament/edit.js
@@ -1,13 +1,61 @@
 let { CommandInteraction } = require("discord.js");
+const firebase = require("../../../firebase");
 module.exports = {
 	/**
 	 *
 	 * @param {CommandInteraction} interaction
 	 */
 	async execute(interaction) {
-		console.log("Edit command deployed!");
 		let options = interaction.options.data[0].options;
 
-		options.forEach();
+		let active_tournament = await firebase.getData(
+			"guilds",
+			interaction.guildId,
+			"tournaments",
+			"active_tournament"
+		);
+		let tournament = await firebase.getData(
+			"guilds",
+			interaction.guildId,
+			"tournaments",
+			active_tournament
+		);
+
+		let acronym = active_tournament;
+		options.forEach((element) => {
+			let prop = element.name;
+			if (prop == "acronym") {
+				acronym = element.value.toUpperCase();
+			} else {
+				tournament.rules[prop] = element.value;
+			}
+		});
+
+		// Clear data in database
+		firebase.setData(
+			{},
+			"guilds",
+			interaction.guildId,
+			"tournaments",
+			active_tournament
+		);
+
+		// Replace data at acronym
+		firebase.setData(
+			tournament,
+			"guilds",
+			interaction.guildId,
+			"tournaments",
+			acronym
+		);
+
+		// Change active tournament to acronym
+		firebase.setData(
+			acronym,
+			"guilds",
+			interaction.guildId,
+			"tournaments",
+			"active_tournament"
+		);
 	},
 };
