f119f6ce6177025a19492cc99cf0ccd0456715a2
clxxiii <earthnoartisjusteh@gmail.com>
2022-04-16 12:09:23 -0400

    better command handler

--- web/api/index.js
+++ web/api/index.js
@@ -1,13 +1,28 @@
-// let test = require("./get_user");
-exports.execute = async (endpoint, req, res) => {
-	// Get file based on endpoint and call execute function
-	try {
-		let command = require("./" + endpoint);
-		await command.execute(req, res);
-	} catch (e) {
-		res.writeHead(500);
-		res.write(e.toString());
-		res.end();
-		return;
-	}
+// Endpoint file handler
+const fs = require("fs");
+
+let endpoints = {};
+let endpointFiles = fs
+	.readdirSync("./web/api/endpoints")
+	.filter((file) => file.endsWith(".js"));
+
+endpointFiles.forEach((element) => {
+	let endpoint = require("./endpoints/" + element);
+	endpoints[endpoint.endpoint] = endpoint;
+});
+
+module.exports = {
+	async execute(endpoint, req, res) {
+		// Get file based on endpoint and call execute function
+		try {
+			let command = endpoints[endpoint];
+			if (!command) return;
+			await command.execute(req, res);
+		} catch (e) {
+			res.writeHead(500);
+			res.write("We encountered an error:" + e);
+			res.end();
+			return;
+		}
+	},
 };
