854f172c13643242b4571549a5eec2e12b6fe909
clxxiii <earthnoartisjusteh@gmail.com>
2022-04-18 15:27:13 -0400

    get_logs temporary endpoint complete

--- web/api/endpoints/get_logs.js
+++ web/api/endpoints/get_logs.js
@@ -4,31 +4,32 @@ let homedir = require("os").homedir();
 module.exports = {
 	endpoint: "get_logs",
 	async execute(req, res) {
+		console.log(req.query);
 		let logs = fs.readFileSync(homedir + "/.pm2/logs/yagami-out.log");
 		logs = logs.toString();
+		if (req.query.type == "text") {
+			res.writeHead(200);
+			res.write(logs);
+			res.end();
+			return;
+		}
+		logs = logs.split("\n");
+		let logList = {};
+		logs.forEach((line) => {
+			let lineRegex = line.match(/(?:(?:\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}))/);
+			let time = lineRegex?.[0];
+			if (time) {
+				let log = line.replace(time + ": ", "");
 
-		// TODO: sort lines into array from based on timestamp
-		// logs = logs.split("\n");
-		// let logList = [];
-		// logs.forEach((line) => {
-		// 	let lineRegex = line.match(
-		// 		/(?:(?:\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}))/
-		// 	);
-		// 	let time = lineRegex?.[0];
-		// 	if (time) {
-		// 		console.log(time);
-		// 		let log = line.replace(time + ": ", "");
-		// 		console.log(logList[time]);
+				if (logList[time] == null) {
+					logList[time] = [log];
+				} else {
+					logList[time].push(log);
+				}
+			}
+		});
 
-		// 		if (logList[time] != null) {
-		// 			logList[time] = [log];
-		// 		} else {
-		// 			logList[time].push(log);
-		// 		}
-		// 	}
-		// });
-
-		res.write(JSON.stringify(logs));
+		res.write(JSON.stringify(logList));
 		res.end();
 	},
 };
