f14b8396b98e87190e3d020c0a9283a8473ddc31
clxxiii <earthnoartisjusteh@gmail.com>
2022-04-27 15:57:56 -0400

    unneccessary import

--- misc/delcommands.js
+++ misc/delcommands.js
@@ -1,15 +1,13 @@
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
-const fs = require("fs");
 require("dotenv").config();
 
 let guildId = "611064433623695370";
 
 const rest = new REST({ version: "9" }).setToken(process.env.discordToken);
 
-rest
-	.put(Routes.applicationGuildCommands(process.env.clientId, guildId), {
-		body: [],
-	})
+rest.put(Routes.applicationGuildCommands(process.env.clientId, guildId), {
+	body: [],
+})
 	.then(() => console.log("Deleted command(s) in " + guildId))
 	.catch(console.error);
