1c596d57a244d178a0b9a602986b5f452eede2d5
clxxiii <earthnoartisjusteh@gmail.com>
2022-04-28 00:59:42 -0400

    send message on join

--- discord/join.js
+++ discord/join.js
@@ -1,10 +1,16 @@
 let deploy = require("./deploy-commands");
-const { GuildMember } = require("discord.js");
+const { GuildMember, MessageEmbed } = require("discord.js");
 const { prisma } = require("../prisma");
+const { stripIndents } = require("common-tags/lib");
 
 module.exports = {
+	/**
+	 *
+	 * @param {import("discord.js").Guild} guild
+	 */
 	async onGuildJoin(guild) {
 		deploy.deployCommands(guild);
+
 		let tournament = await prisma.guild.findMany({
 			where: {
 				guild_id: guild.id,
@@ -22,6 +28,53 @@ module.exports = {
 				})
 				.catch((err) => console.log(err));
 		}
+		let embed = new MessageEmbed()
+			.setTitle("Thanks for the invite!")
+			.setThumbnail("https://yagami.clxxiii.dev/static/yagami%20var.png")
+			.setColor("#F88000")
+			.setDescription(
+				stripIndents`
+				Welcome to Yagami, the future of osu! tournaments.
+				Yagami is many things, a discord bot, an auto ref, and even a website!
+				Over the course of the past 2 months, this bot has continuously been in development
+				I'll add more to this when I have something more profound to say
+
+				**Let's get your server up and running:**
+				`
+			)
+			.addField("Set up your server settings:", `\`\`\`/settings\`\`\``)
+			.addField(
+				"Link your account to your osu! account:",
+				`\`\`\`/link\`\`\``
+			)
+			.addField(
+				"Set up your first tournament:",
+				`\`\`\`/tournaments create\`\`\``
+			)
+			.setTimestamp()
+			.setFooter({
+				text: "Made with ❤️ by clxxiii#8958",
+				iconURL: "https://clxxiii.dev/img/icon.png",
+			});
+
+		let permissionsEmbed = new MessageEmbed()
+			.setTitle("⚠️ Permissions Warning ⚠️")
+			.setColor("DARK_ORANGE")
+			.setImage("https://i.imgur.com/nLd71Ai.png")
+			.setDescription(
+				"A recent update to discord has changed the way slash command permissions work. Currently, I can't the permissions for you, and you wouldn't want players deleting your tournament, would you?"
+			)
+			.addField(
+				"To change slash command settings:",
+				"Go into your server settings, select integrations, and then click on Yagami to edit settings."
+			);
+		let channel = guild.channels.cache.find(
+			(channel) =>
+				channel.type === "GUILD_TEXT" &&
+				channel.permissionsFor(guild.me).has("SEND_MESSAGES")
+		);
+		channel = guild.systemChannel || channel;
+		channel.send({ embeds: [embed, permissionsEmbed] });
 	},
 	/**
 	 *
@@ -45,7 +98,13 @@ module.exports = {
 
 		let linkedRoleObj = member.guild.roles.cache.get(linkedRole);
 		if (!linkedRoleObj)
-			await setData(null, "guilds", member.guild.id, "settings", "linked_role");
+			await setData(
+				null,
+				"guilds",
+				member.guild.id,
+				"settings",
+				"linked_role"
+			);
 		else await member.roles.add(linkedRoleObj);
 	},
 };
