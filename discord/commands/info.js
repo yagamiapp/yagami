e31e3783db0ac855263b5528ae189df3da5b68a9
clxxiii <earthnoartisjusteh@gmail.com>
2022-04-28 00:23:40 -0400

    better looking info

--- discord/commands/info.js
+++ discord/commands/info.js
@@ -2,6 +2,8 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { stripIndents } = require("common-tags/lib");
 const { MessageEmbed } = require("discord.js");
+const { version } = require("../../package.json");
+const { prisma } = require("../../prisma");
 const { "discord.js": djs } = require("../../package.json").dependencies;
 const { start_time } = require("../../index");
 
@@ -14,6 +16,7 @@ module.exports = {
 	 * @param {import("discord.js").CommandInteraction} interaction
 	 */
 	async execute(interaction) {
+		await interaction.deferReply();
 		let uptime = Date.now() - start_time;
 		let uptimeSplits = {
 			days: Math.floor(uptime / (1000 * 60 * 60 * 24)),
@@ -22,32 +25,88 @@ module.exports = {
 			secs: Math.floor((uptime % (1000 * 60)) / 1000),
 		};
 		let uptimeString =
-			(uptimeSplits.days != 0 ? `${uptimeSplits.days} days, ` : "") +
-			(uptimeSplits.hours != 0 ? `${uptimeSplits.hours} hrs, ` : "") +
-			(uptimeSplits.mins != 0 ? `${uptimeSplits.mins} mins, ` : "") +
+			(uptimeSplits.days != 0 ? `${uptimeSplits.days} days ` : "") +
+			(uptimeSplits.hours != 0 ? `${uptimeSplits.hours} hrs ` : "") +
+			(uptimeSplits.mins != 0 ? `${uptimeSplits.mins} mins ` : "") +
 			(uptimeSplits.secs != 0 ? `${uptimeSplits.secs} secs` : "");
+
+		// TODO: Right align text
+		let tournaments = await prisma.tournament.count({});
+		let info = {
+			Version: [
+				{ name: "Bot Version", value: version },
+				{ name: "Node Version", value: process.versions.node },
+				{ name: "DJS Version", value: djs },
+			],
+			Latency: [
+				{ name: "Uptime", value: uptimeString },
+				{
+					name: "Response Time",
+					value: Date.now() - interaction.createdTimestamp + " ms",
+				},
+				{
+					name: "API Latency",
+					value: Math.round(interaction.client.ws.ping) + " ms",
+				},
+			],
+			Statistics: [
+				{ name: "Servers", value: interaction.client.guilds.cache.size },
+				{ name: "Tournaments", value: tournaments },
+			],
+		};
+
+		let maxLength = 48;
+		for (const key in info) {
+			const element = info[key];
+		}
 
 		let embed = new MessageEmbed()
 			.setColor("#F88000")
 			.setTitle("Yagami")
+			.setDescription("A bot for managing tournaments")
 			.setThumbnail("https://yagami.clxxiii.dev/static/yagami%20var.png")
-			.setDescription(
-				stripIndents`	
-
-					> Response Time ➣ **${Date.now() - interaction.createdTimestamp}ms**
-					> API Latency ➣ **${Math.round(interaction.client.ws.ping)} ms**
-					> Host ➣ **DigitalOcean**
-					> Uptime ➣ **${uptimeString}**
-					> Node Version ➣ **${process.versions.node}**
-					> DJS Version ➣ **${djs}**
-					> Servers ➣ **${interaction.client.guilds.cache.size}**
+			.addField(
+				"Version",
+				stripIndents`
+				\`\`\`js
+					➣ Bot Version    ${version}
+					➣ Node Version   ${process.versions.node}
+					➣ DJS Version    ${djs}
+				\`\`\`
+				`
+			)
+			.addField(
+				"Latency",
+				stripIndents`
+				\`\`\`js
+					➣ Uptime         ${uptimeString}
+					➣ Response Time  ${Date.now() - interaction.createdTimestamp} ms
+					➣ API Latency    ${Math.round(interaction.client.ws.ping)} ms
+					\`\`\`
+					`
+			)
+			.addField(
+				"Statistics",
+				stripIndents`
+				\`\`\`js
+					➣ Servers        ${interaction.client.guilds.cache.size}
+					➣ Tournaments    ${tournaments}
+				\`\`\`
+				`
+			);
+		embed
+			.addField(
+				"Developer",
+				stripIndents`
+				<@265144290240495617>
+				[Discord](no) | [Twitter](https://twitter.com/clxxiii1) | [GitHub](https://github.com/clxxiii) | [osu!](https://osu.ppy.sh/users/10962678)
 				`
 			)
 			.setTimestamp()
 			.setFooter({
-				text: "Developed by clxxiii#8958",
+				text: "Made with ❤️",
 				iconURL: "https://clxxiii.dev/img/icon.png",
 			});
-		await interaction.reply({ embeds: [embed] });
+		await interaction.editReply({ embeds: [embed] });
 	},
 };
