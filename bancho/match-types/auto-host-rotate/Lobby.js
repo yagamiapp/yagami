30d25bb2f08587650172d048c96f14700b8c8429
clxxiii <earthnoartisjusteh@gmail.com>
2022-05-15 22:14:46 -0400

    lobby name

--- bancho/match-types/auto-host-rotate/Lobby.js
+++ bancho/match-types/auto-host-rotate/Lobby.js
@@ -18,6 +18,12 @@ class Lobby {
 				discordId: this.owner_id,
 			},
 		});
+		this.max_length = lobby.max_length;
+		this.max_rank = lobby.max_rank;
+		this.max_stars = lobby.max_stars;
+		this.min_length = lobby.min_length;
+		this.min_rank = lobby.min_rank;
+		this.min_stars = lobby.min_stars;
 
 		if (!lobby) {
 			throw "User does not own a lobby";
@@ -35,6 +41,7 @@ class Lobby {
 		}
 		await this.lobby.clearHost();
 		await this.lobby.updateSettings();
+		this.lastMap = this.lobby.beatmap;
 
 		this.channel.on("message", async (msg) => {
 			await msgHandler(msg);
@@ -45,6 +52,29 @@ class Lobby {
 		this.lobby.on("playerLeft", async (player) => {
 			await leaveHandler(player);
 		});
+		this.lobby.on("beatmap", async (beatmap) => {
+			console.log(this.lobby.beatmap);
+			await mapHandler(this.lastMap, beatmap);
+		});
+		// Make lobby name
+		let nameString = "AHR |";
+		if (this.min_rank != null || !his.max_rank != 1) {
+			nameString += ` (#${
+				this.min_rank.toLocaleString() ?? "∞"
+			} - #${this.max_rank.toLocaleString()})`;
+		}
+		if (this.min_stars != 0 || this.max_stars != null) {
+			nameString += ` (${this.min_stars.toFixed(2)}☆ - ${
+				this.max_stars.toFixed(2) + "☆" ?? "∞"
+			})`;
+		}
+		if (this.min_length != 0 || this.max_length != null) {
+			nameString += ` (${secondsToTime(this.min_length)} - ${
+				secondsToTime(this.max_length) ?? "∞"
+			})`;
+		}
+
+		this.channel.sendMessage(`!mp name ${nameString}`);
 
 		/**
 		 * Set up player queue
@@ -63,7 +93,6 @@ class Lobby {
 		let slotMap = this.lobby.slots
 			.filter((x) => x)
 			.map((slot) => slot.user.id);
-		console.log("Player Map", playerMap);
 		for (const player of playerMap) {
 			if (!slotMap.includes(player)) {
 				await prisma.autoHostRotatePlayer.delete({
@@ -81,8 +110,6 @@ class Lobby {
 				await this.addPlayer(player);
 			}
 		}
-
-		console.log(this.queue);
 	}
 
 	/**
@@ -120,5 +147,11 @@ async function joinHandler(player) {
  * @param {import("bancho.js").BanchoLobbyPlayer} player
  */
 async function leaveHandler(player) {}
+
+function secondsToTime(seconds) {
+	let minutes = Math.floor(seconds / 60);
+	seconds = Math.floor((seconds % 60) % 60);
+	return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+}
 
 module.exports.Lobby = Lobby;
