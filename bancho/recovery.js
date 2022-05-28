cc73397abdaacbbf3ab3884b8e07f753f802f14a
clxxiii <earthnoartisjusteh@gmail.com>
2022-05-27 23:54:50 -0400

    slight recovery change

--- bancho/recovery.js
+++ bancho/recovery.js
@@ -1,18 +1,14 @@
 const { prisma } = require("../prisma");
 const { MatchManager } = require("./match-types/bracket/Match");
 module.exports.recover = async () => {
-	let matches = await prisma.match.findMany({
-		where: {
-			state: {
-				not: -1,
-			},
-		},
-	});
+	let matches = await prisma.match.findMany({});
 
 	for (const match of matches) {
-		if (match.state < 8) {
+		if (match.state < 8 && match.state != -1) {
 			let manager = new MatchManager(match.id, match.mp_link);
 			await manager.createMatch();
+		} else if (match.state == -1) {
+			//TODO: Delete match
 		}
 	}
 };
