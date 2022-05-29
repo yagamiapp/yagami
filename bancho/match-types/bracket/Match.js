1349df886ba81202bc1f28f40fb2f683fd7daedb
clxxiii <earthnoartisjusteh@gmail.com>
2022-05-28 20:17:24 -0400

    actual chimu.moe mirror

--- bancho/match-types/bracket/Match.js
+++ bancho/match-types/bracket/Match.js
@@ -879,7 +879,7 @@ class MatchManager {
 
 		await this.addPick(map);
 		await this.channel.sendMessage(
-			`${team.name} chooses ${map.identifier} | [https://osu.ppy.sh/b/${map.beatmapId} ${map.artist} - ${map.title} [${map.version}]] - [https://beatconnect.io/b/${map.beatmapset_id} Beatconnect Mirror] - [https://beatconnect.io/b/${map.beatmapset_id} chimu.moe Mirror]`
+			`${team.name} chooses ${map.identifier} | [https://osu.ppy.sh/b/${map.beatmapId} ${map.artist} - ${map.title} [${map.version}]] - [https://beatconnect.io/b/${map.beatmapset_id} Beatconnect Mirror] - [https://api.chimu.moe/v1/download/${map.beatmapset_id} chimu.moe Mirror]`
 		);
 	}
 	/**
