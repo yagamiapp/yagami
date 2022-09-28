8eea2eadd8344fb01acbd1daeccf6b2bdccf03bc
clxxiii <earthnoartisjusteh@gmail.com>
2022-09-27 23:40:55 -0400

    change dependencies for deploy

--- src/discord/deploy-commands.js
+++ src/discord/deploy-commands.js
@@ -1,5 +1,5 @@
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
+const { REST, Routes } = require("discord.js");
+// const { Routes } = require("discord-api-types/v9");
 const fs = require("fs");
 
 /**
