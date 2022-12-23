083f353073539ecc1bbd6c7c92c27ee6a503e0c6
clxxiii <clxxiii@users.noreply.github.com>
2022-12-23 14:29:05 -0500

    scrims subcommand handler

---
+++ src/discord/commands/scrims.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require("discord.js");
+const fs = require("fs");
+
+// Subcommand Handler
+let data = new SlashCommandBuilder()
+	.setName("scrims")
+	.setDescription("Manage scrims");
+let subcommands = {};
+
+const subcommandFiles = fs
+	.readdirSync("./src/discord/commands/round")
+	.filter((file) => file.endsWith(".js"));
+
+for (const file of subcommandFiles) {
+	const subcommand = require(`./round/${file}`);
+	data.addSubcommand(subcommand.data);
+	subcommands[subcommand.data.name] = subcommand;
+}
+
+module.exports = {
+	data,
+	async execute(interaction) {
+		let subcommand = interaction.options.getSubcommand();
+		let file = subcommands[subcommand];
+		await file.execute(interaction);
+	},
+};
